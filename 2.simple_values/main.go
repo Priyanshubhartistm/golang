68f8c5a83f27a3678174482017711b57374ea05d
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-08-19 00:19:48 +0530

    main

---
+++ 2.simple_values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	// integer
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("hello golang")
+
+	// boolean
+	fmt.Println("true && false") // Note: This line has a typo, it should be fmt.Println("true && false")
+
+	// float
+	fmt.Println(3.14 + 2.71)
+
+}
