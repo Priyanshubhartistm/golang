672c61947c4579165ccd8a21b720bd1d45550b7a
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-08-19 10:28:19 +0530

    variable

---
+++ 3.variables/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// without declaring type
+	//var name = "priya"
+	//var name string = "priya"
+	//fmt.Println(name)
+
+	// number
+	// var age int = 25
+	// fmt.Println(age)
+
+	// boolean
+	// var isAdult bool = true
+	// fmt.Println(isAdult)
+
+	// shorthand syntax
+	// name := "golang"   // ek time pr variables ko declare krna or assign v krna h, to
+	// fmt.Println(name)
+
+	// 1. var name string
+	// name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	price := 50.5
+
+	fmt.Println(price)
+
+}
