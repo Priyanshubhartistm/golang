b4769e037333e3611109cb6c33344adcc475ec98
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-08-23 20:19:18 +0530

    for

--- 5.for/for.go
+++ 5.for/for.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-
-// go andr bs for loop hi h, while loop v ni h, for loop ki help se hi while loop ko implement krte h.
-
 // for -> only construct in go for looping
 func main() {
 
@@ -12,7 +8,6 @@ func main() {
 	// for i <= 3 {
 	// 	fmt.Println(i)
 	// 	i = i + 1
-
 	// }
 
 	// infinite loop
@@ -21,12 +16,7 @@ func main() {
 	// }
 
 	// classic for loop
-	for i := 0; i <= 3; i++ {}
+	// for i := 0; i <= 3; i++ {
+	// 	// break
 
-	// break
-
-	// 	if i = 2 {
-	// 		continue
-	// 	}
-
-	
+}
