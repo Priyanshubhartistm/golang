d53530a0e96db3983a000f3c0840bc17c50c5c1c
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-05 22:05:46 +0530

    constant

--- 4.constants/constants.go
+++ 4.constants/constants.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// constants = aise variables h, jinka value change ni hota h.
+	// const keyword se declare krte h.
+	// const ka value hum kabhi change ni kr skte h.
+	// const ka value hum program ke andar kahi bhi use kr skte h.
+
 	// const name = "golang"
 	// const age = 30
 
