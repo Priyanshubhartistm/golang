88c220c26d654052cae7c2c01440b2173aed81f0
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-05 22:22:48 +0530

    for

---
+++ 5.for/for.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// for -> only construct in go for looping
+// for ke madad se hi while loop aur infinite loop implement kr skte h
+
+func main() {
+
+	// while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+	// for {
+	// 	println("1")
+	// }
+
+	// classic for loop
+	for i := 0; i <= 3; i++ {
+		fmt.Println(i)
+		//  break = loop ko stop krne ke liye use hota h,
+
+	}
+}
