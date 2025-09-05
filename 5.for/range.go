8561c0a6946cfb21d5483f3ae5babe7a8a4a6d09
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-05 22:41:55 +0530

    range

---
+++ 5.for/range.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := range 3 {
+		fmt.Println(i)
+
+	}
+}
+
+// go 22 ke andar ek new feature aaya h, jo ki h, "for range"
+// for range ka use hum slice, array, map, string ke upar loop chalane ke liye krte h
