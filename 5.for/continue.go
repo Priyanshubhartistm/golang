7e5f762d32bd9c125b3f5137fe7c36221fbb3025
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-08 21:09:23 +0530

    continue

--- 5.for/continue.go
+++ 5.for/continue.go
@@ -4,17 +4,15 @@ import "fmt"
 
 func main() {
 	for i := 0; i <= 3; i++ {
-		// break = loop ko stop krne ke liye use hota h,
+		// break = loop ko stop krne ke liye use hota h
 		if i == 2 {
 			continue
-
-			// 		continue = continue keyword h, o current iteration ko skip kr deta h, jaise:- (2) print ni hoga direct, (0,1,3) print ho jayega.
-			// continue keyword loop ke andar hi use hota h.
-			// continue keyword loop ke bahar use ni hota h.
-
 		}
-		fmt.Println(i)
 
+		// continue = continue keyword h, o current iteration ko skip krta h
+		// continue keyword loop ke andar hi use hota h
+		// continue keyword loop ke bahar use ni hota h
+
+		fmt.Println(i) // ✅ move this line inside the loop
 	}
-
 }
