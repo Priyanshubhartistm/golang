00e9a38f37a2b48bda6b819851618ede2ac71558
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-08 21:46:42 +0530

    logical

--- 6.if_else/logical.go
+++ 6.if_else/logical.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main() {
 	var role = "admin"
-	var hasPermission = true
-
+	var hasPermission = false
 	// if role == "admin" || hasPermission {
 	// 	// == double equal to use hota h, compare krne k liye
 	// 	fmt.Println("yes")
+	// || means or
+
 	// }
 
 	if role == "admin" && hasPermission {
-
 		fmt.Println("yes")
+
+		// && means and
 
 	}
 
