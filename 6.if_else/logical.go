619f8836efbe4cceb2d5a0ebadf1108528188bb7
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-08 21:42:29 +0530

    log

--- 6.if_else/logical.go
+++ 6.if_else/logical.go
@@ -6,8 +6,15 @@ func main() {
 	var role = "admin"
 	var hasPermission = true
 
-	if role == "admin" || hasPermission {
-		// == double equal to use hota h, compare krne k liye
+	// if role == "admin" || hasPermission {
+	// 	// == double equal to use hota h, compare krne k liye
+	// 	fmt.Println("yes")
+	// }
+
+	if role == "admin" && hasPermission {
+
 		fmt.Println("yes")
+
 	}
+
 }
