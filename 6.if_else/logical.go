0752ff56e0fde5fea810594af9350aaf88fb2947
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-08 21:36:18 +0530

    logical

---
+++ 6.if_else/logical.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	var role = "admin"
+	var hasPermission = true
+
+	if role == "admin" || hasPermission { // == double equal to use hota h, compare krne k liye
+		println("yes")
+	}
+}
