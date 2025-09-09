7ff7ea05d8659af4a603419d961281b385bed3d2
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-10 01:37:51 +0530

    type

--- 7.switch/type.go
+++ 7.switch/type.go
@@ -2,25 +2,26 @@
 
 package main
 
+import "fmt"
+
 func main() {
 
-whoAmI := func(i interface{}) {
+	whoAmI := func(i interface{}) {
 
-	switch t := i.(type) {
+		switch t := i.(type) {
 
-	case int:
-		fmt.Println("its an integer")
+		case int:
+			fmt.Println("its an integer")
 
-	case string:
-		fmt.Println("its a string")
+		case string:
+			fmt.Println("its a string")
 
+		case bool:
+			fmt.Println("its a boolean")
+
+		default:
+			fmt.Println("other")
+
+		}
 	}
-
-	case bool:
-		fmt.Println("its a boolean")
-
-default:
-	fmt.Println("other")
-}
-
 }
