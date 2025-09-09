35db10d44644a1efbfceca46cea1c5758c51e833
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-09 20:00:13 +0530

    switch

---
+++ 7.switch/switch.go
@@ -0,0 +1,31 @@
+// SWITCH = jab bhut jda conditions hote h, to ham switch k use krte h
+// IF,ELSE = kam me if,else k kar lete h
+
+package main
+
+import "fmt"
+
+func main() {
+	// simple switch
+	i := 3
+
+	switch i {
+
+	case 1:
+		fmt.Println("one")
+
+	case 2:
+		fmt.Println("two")
+
+	case 3:
+		fmt.Println("three")
+
+	case 4:
+		fmt.Println("four")
+
+		// default:
+		// 	fmt.Println("other")
+
+	}
+
+}
