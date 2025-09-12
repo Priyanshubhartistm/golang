1aec79a8a398805aa32badf4b022a0d5032d92a7
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-13 00:20:00 +0530

    nums

---
+++ 8.array/2.num.go
@@ -0,0 +1,16 @@
+//  any element add
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var nums [4]int
+	nums[0] = 1
+	fmt.Println(nums[0])
+
+	//if you want to print entire array
+	//fmt.Println(nums)
+
+}
