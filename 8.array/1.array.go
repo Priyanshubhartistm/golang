f11e339ced1f5a93010be9da13f98fa62272111e
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-12 23:52:33 +0530

    array

---
+++ 8.array/1.array.go
@@ -0,0 +1,17 @@
+// ARRAY = numbered sequence of specific length
+// ARRAY = collection of items of same type
+// ARRAY = fixed size
+// ARRAY = indexed from 0 to n-1
+// ARRAY = stored in contiguous memory locations
+// ARRAY = can be multi-dimensional
+
+package main
+
+import "fmt"
+
+func main() {
+	var nums [4]int
+
+	fmt.Println(len(nums))
+
+}
