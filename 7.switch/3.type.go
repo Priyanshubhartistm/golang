cdbd9066fbc01da90608f5b11acc379f3d6f676b
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-12 17:59:24 +0530

    type

---
+++ 7.switch/3.type.go
@@ -0,0 +1,30 @@
+// type switch
+
+package main
+
+import "fmt"
+
+func main() {
+
+	whoAmI := func(i interface{}) {
+
+		switch t := i.(type) {
+
+		case int:
+			fmt.Println("its an integer")
+
+		case string:
+			fmt.Println("its a string")
+
+		case bool:
+			fmt.Println("its a boolean")
+
+		default:
+			fmt.Println("other, t")
+
+		}
+	}
+
+	whoAmI("golang")
+
+}
