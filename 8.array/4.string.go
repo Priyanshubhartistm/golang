11cd77348880db4ccbcd7fa691db89b220b8b5ee
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-13 00:35:57 +0530

    string

--- 8.array/4.string.go
+++ 8.array/4.string.go
@@ -13,3 +13,10 @@ func main() {
 	fmt.Println(name)
 
 }
+
+// if values kch nahi hai to use case me value kya print hoga dekhte h
+// 0 for int
+// false for bool
+// "" for string
+// 0.0 for float
+// nil for pointer
