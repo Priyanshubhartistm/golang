5e7592f2f0a2d6bb2836bfb3c6d282f20a386a66
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-24 22:44:24 +0530

    capacity

--- 9.slices/2.cap.go
+++ 9.slices/2.cap.go
@@ -1,4 +1,4 @@
-// capacity = in go maxium no. of elements can fit
+// capacity = in go maxium no. of elements can fit , ise slice ki capacity kitni h, 2 iska matlb y ni h, ki 2 hi no. fit honge iske andr oo
 
 package main
 
@@ -8,5 +8,6 @@ func main() {
 
 	var nums = make([]int, 20)
 
+	// yaha pe ek inbuilt method ati h, jo ki h,cap; or (nums) ko iske andr pass krenge or
 	fmt.Println(cap(nums))
 }
