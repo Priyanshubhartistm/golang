8902c497f83b80bc50b958f279b13a6ad7ab0c4b
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-26 22:06:50 +0530

    anotherslices

--- 9.slices/4.another.go
+++ 9.slices/4.another.go
@@ -5,7 +5,10 @@ import "fmt"
 
 func main() {
 
-	nums := []int{}
+	nums := []int{1, 2, 3, 4, 5}
+
+	// if i add append
+	nums = append(nums, 1)
 
 	fmt.Println(nums)      // output = [1 2 3 4 5]
 	fmt.Println(cap(nums)) // output = 5
