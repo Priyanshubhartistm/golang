2a994e02b1514e0a3f0c4c792c157a4927a56609
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-26 21:49:20 +0530

    add

--- 9.slices/3.add.go
+++ 9.slices/3.add.go
@@ -5,7 +5,7 @@ import "fmt"
 
 func main() {
 
-	var nums = make([]int, 2, 5)
+	// var nums = make([]int, 2, 5)
 	var nums = make([]int, 0, 5) // normally yaha pe starting me 0 rakha jata h, q ki yaha p new slices add krni h, to yaha pe 0 rakha jata h, taki starting me jo do br 0 ata h, oo n aa jaye.
 
 	nums = append(nums, 1)
