935a50abf685419e36ef2ae8c1463aaed12b6fe7
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-26 22:05:10 +0530

    anotherslices

--- 9.slices/4.another.go
+++ 9.slices/4.another.go
@@ -5,7 +5,9 @@ import "fmt"
 
 func main() {
 
-	nums := []int{1, 2, 3, 4, 5}
+	nums := []int{}
 
-	fmt.Println(nums) // output = [1 2 3 4 5]
+	fmt.Println(nums)      // output = [1 2 3 4 5]
+	fmt.Println(cap(nums)) // output = 5
+	fmt.Println(len(nums)) // output = 5
 }
