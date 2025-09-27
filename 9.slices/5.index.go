db9d75f0143f24c926aed38d57b40e5cdfd69f21
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-27 21:51:47 +0530

    index

---
+++ 9.slices/5.index.go
@@ -0,0 +1,14 @@
+// for example apko index ki help se iske andr element add krna h, to aap is tarike se kr skte h
+package main
+
+import "fmt"
+
+func main() {
+	var nums = make([]int, 2, 4)
+
+	nums[0] = 3
+	nums[0] = 5
+
+	fmt.Println(nums)
+
+}
