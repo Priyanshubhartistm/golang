59c2eec581f3e59997cd973bc6bf96cc56eae49e
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-27 22:10:17 +0530

    Copy function

--- 9.slices/6.copy.go
+++ 9.slices/6.copy.go
@@ -1,2 +1,19 @@
 // Copy function in slices
 
+package main
+
+import "fmt"
+
+func main() {
+
+	var nums = make([]int, 0, 5)
+
+	nums = append(nums, 2)
+	var nums2 = make([]int, len(nums))
+
+	// copy function
+	copy(nums2, nums)
+
+	fmt.Println(nums, nums2)
+
+}
