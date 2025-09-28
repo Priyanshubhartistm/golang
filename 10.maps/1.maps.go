aeb5f82ae6ac7afcc04fc480d0a26aa083b49a00
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-09-28 22:12:19 +0530

    map

---
+++ 10.maps/1.maps.go
@@ -0,0 +1,20 @@
+// Maps  = Associated data structure hota h.
+// if you come to another language thn you knoe to, jo Hash table, js ke andr obj, python ke andr dictionary hote h,
+// to usi trh k data structure chahiya to apko maps use krna hoga
+package main
+
+import "fmt"
+
+func main() {
+
+	// creating map
+
+	m := make(map[string]string)
+
+	// setting an element
+
+	m["name"] = "golang"
+
+	// get an element a
+	fmt.Println(m["name"])
+}
