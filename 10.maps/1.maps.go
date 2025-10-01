ba704d1e4cceef5a3adad6a4c50603bed2e87335
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-01 22:22:56 +0530

    map

--- 10.maps/1.maps.go
+++ 10.maps/1.maps.go
@@ -1,6 +1,7 @@
 // Maps  = Associated data structure hota h.
 // if you come to another language thn you knoe to, jo Hash table, js ke andr obj, python ke andr dictionary hote h,
 // to usi trh k data structure chahiya to apko maps use krna hoga
+// maps are reference types
 
 package main
 
