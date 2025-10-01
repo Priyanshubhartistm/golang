285f515c6ac5085970a75d8e8b7edd9ba3c21b03
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-02 00:04:19 +0530

    withoutmake

---
+++ 10.maps/5.withoutmake.go
@@ -0,0 +1,13 @@
+// without make function in maps
+
+package main
+
+import "fmt"
+
+func main() {
+
+	m := map[string]int{"price": 40, "phone": 100} // without make function in
+
+	fmt.Println(m) // map[phone:100 price:40]
+
+}
