3dcf9f71516ba93fa2acf764c67351e60c3d4a6e
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-03 00:11:22 +0530

    function

---
+++ 12.functions/1.functions.go
@@ -0,0 +1,32 @@
+// functions in go lang
+// function is a block of code that performs a specific task
+// function is a reusable code block
+// function is a way to organize code
+// function is a way to modularize code
+// function is a way to improve code readability
+// function is a way to improve code maintainability
+// function is a way to improve code reusability
+
+package main
+
+import "fmt"
+
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+// multiple return values in function
+func getlanguage() (string, string, string) {
+	return "golang", "python", "javascript"
+}
+
+func main() {
+	// result := add(3, 5)
+	// println(result) // 8
+
+	fmt.Println((getlanguage())) // golang python javascript
+
+	// kise values ko agr ham use ni krte h, to ham _ (underscore) ka use kr skte h
+	l1, l2, _ := getlanguage()
+	fmt.Println(l1, l2) // golang python
+}
