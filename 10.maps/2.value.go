8c0c89d3a3bfd6b78a970305eabf6d14a974e062
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-02 13:00:18 +0530

    values

--- 10.maps/2.value.go
+++ 10.maps/2.value.go
@@ -9,10 +9,12 @@ func main() {
 
 	m["age"] = 30
 	m["price"] = 50
+
 	//fmt.Println(m["phone"])
 	// phone get krne ki kosis krenge, jo ki map ke andr exists hi ni krta h, to 0 values dega by default
 	// "zero" value diya bcz "int" type ki values h. agr boolean hota to "false" values deta.
 
-	fmt.Println(len(m)) // 1 length
+	// fmt.Println(len(m)) // 1 length
+
 	fmt.Println(len(m)) // 2 length
 }
