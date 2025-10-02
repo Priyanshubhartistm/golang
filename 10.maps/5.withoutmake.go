ffb9d41c0cbf0ac90e48464b3c8593c6685b9b0e
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-02 13:02:56 +0530

    without

--- 10.maps/5.withoutmake.go
+++ 10.maps/5.withoutmake.go
@@ -1,5 +1,9 @@
 // without make function in maps
 
+// if you have a already elements avilable from starting, then
+
+// if you have not avilable elements from starting, then use make function
+
 package main
 
 import "fmt"
