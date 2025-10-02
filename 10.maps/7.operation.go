dd2c304b672386f5ab851f0a5a6c74587d0db0f2
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-02 13:03:40 +0530

    operator

--- 10.maps/7.operation.go
+++ 10.maps/7.operation.go
@@ -1,5 +1,5 @@
 // operation in maps
-
+// how to check equality in maps
 package main
 
 import (
@@ -15,3 +15,8 @@ func main() {
 	fmt.Println(maps.Equal(m1, m2)) // true
 
 }
+
+// output true, because both maps are equal
+// if both maps are not equal, then output false
+// maps.Equal function use krke hum check kr skte h ki dono maps equal h ki ni
+// maps.Equal function use krne ke liye "maps" package import krna padta h
