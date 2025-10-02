edf1d70b6d65f18c904f89eaea472be2ece90401
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-03 00:09:41 +0530

    sum

---
+++ 11.range/2.sum.go
@@ -0,0 +1,21 @@
+// sum of elements in a range
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{6, 7, 8}
+
+	sum := 0
+
+	for _, num := range nums {
+
+		sum = sum + num
+
+	}
+
+	// println(num)
+	fmt.Println(sum)
+
+}
