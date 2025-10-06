5b311db7c2965e69cd8ee7e5a859f82c0403d4e2
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-06 22:31:11 +0530

    struct

---
+++ 16.Structs/1.Structs.go
@@ -0,0 +1,4 @@
+// struct in go = it is a basically coustm data structure hote h,
+// jaise dusri programming ke andr classes bante h, or obj. banake usko use krte h, to kch isi trh se ham go ke andr class ki jhug "struct" use kar skte h
+
+package main
