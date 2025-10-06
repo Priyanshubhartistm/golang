db84789d2a2de51b902e4fa67e875de034b34581
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-10-06 22:36:44 +0530

    struct

--- 16.Structs/1.Structs.go
+++ 16.Structs/1.Structs.go
@@ -2,3 +2,12 @@
 // jaise dusri programming ke andr classes bante h, or obj. banake usko use krte h, to kch isi trh se ham go ke andr class ki jhug "struct" use kar skte h
 
 package main
+
+
+
+struct 
+
+
+fuc main() {
+
+}
